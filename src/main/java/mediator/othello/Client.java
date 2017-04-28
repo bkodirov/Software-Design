d17dffb7d0ef1db0558ec7c343c7480b5e15d029
Beka <q.bekki@gmail.com>
2017-04-28 02:27:59 -0500

    game board for mediator created

---
+++ src/main/java/mediator/othello/Client.java
@@ -0,0 +1,23 @@
+package mediator.othello;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by Beka on 28.04.17.
+ */
+public class Client extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/othello/Othello.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/mediator/othello/Othello.java
@@ -0,0 +1,138 @@
+package mediator.othello;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Othello extends ImageGridGame<Boolean> {
+
+	@FXML
+	Label statusLabel;
+	
+	@FXML
+	protected void initialize() {
+		super.initialize();
+		imageGrid.setImage(true, "/othello/white.png", this);
+		imageGrid.setImage(false, "/othello/black.png", this);
+		imageGrid.setImage(null, "/othello/empty.png", this);
+		newAction();
+	}
+
+	Boolean player = true;
+	
+	@FXML
+	void newAction() {
+		fillGrid(null);
+		setCell(3, 3, true);
+		setCell(3, 4, false);
+		setCell(4, 3, false);
+		setCell(4, 4, true);
+		player = true;
+		updateStatus();
+	}
+
+	String getPlayerName(boolean player) {
+		return player ? "White" : "Black";
+	}
+	
+	int playerPossibilities = -1, otherPossibilities = -1;
+
+	private void updateStatus() {
+		int whitePoints = countPieces(true), blackPoints = countPieces(false);
+		String status = "White has " + whitePoints + " pieces, Black has " + blackPoints + " pieces.";
+		if (player == null) {
+			if (whitePoints > blackPoints) {
+				status = status + " White won!";
+			} else if (blackPoints > whitePoints) {
+				status = status + " Black won!";
+			} else {
+				status = " It is a draw...";
+			}
+		} else {
+			if (countPossibilities(! player) == 0) {
+				status = status + getPlayerName(! player) + " must pass. ";
+			}
+			status = status + " " + getPlayerName(player) + "'s turn.";
+		}
+		statusLabel.setText(status);
+	}
+
+	private void nextPlayer() {
+		if (countPossibilities(! player) > 0) {
+			player = ! player;
+		}
+		if (countPossibilities(player) == 0) {
+			player = null;
+		}
+	}
+
+	int countPieces(boolean player) {
+		return countCellsIf((x, y) -> Boolean.valueOf(player) == getCell(x, y));
+	}	
+
+	int countPossibilities(boolean player) {
+		return countCellsIf((x, y) -> turnPieces(player, x, y, false) > 0);
+	}	
+
+	private boolean turningPieces = false;
+	
+	@Override
+	protected boolean mouseClicked(int x, int y) {
+		if (player == null || turningPieces) {
+			return false;
+		}
+		final int count = turnPieces(player, x, y, false);
+		if (count > 0) {
+			setCell(x, y, player);
+			updateStatus();
+			turningPieces = true;
+//			runDelayed(500,
+//					() -> {
+						turningPieces = false;
+						turnPieces(player, x, y, true);
+						nextPlayer();
+						updateStatus();
+//					}
+//			);
+		}
+		return true;
+	}
+
+	int turnPieces(boolean player, int x, int y, boolean reallyTurn) {
+		int count = 0;
+		if (getCell(x, y) == null || reallyTurn) {
+			for (int dx = -1; dx <=1; dx++) {
+				for (int dy = -1; dy <=1; dy++) {
+					if (dx != 0 || dy != 0) {
+						count = count + turnPieces(player, x + dx, y + dy, dx, dy, reallyTurn);
+					}
+				}
+			}
+		}
+		return count;
+	}
+
+	int turnPieces(boolean player, int x, int y, int dx, int dy, boolean reallyTurn) {
+		int count = 0;
+		while (isValidXY(x + count * dx, y + count * dy)) {
+			Boolean otherCell = getCell(x + count * dx, y + count * dy);
+			if (otherCell == null) {
+				return 0;
+			} else if (otherCell == player) {
+				if (reallyTurn) {
+					for (int i = 0; i < count; i++) {
+						setCell(x + i * dx, y + i * dy, player);
+					}
+				}
+				return count;
+			}
+			count++;
+		}
+		return 0;
+	}
+
+	//
+	
+	public static void main(String[] args) {
+//		launch(Othello.class);
+	}
+}
